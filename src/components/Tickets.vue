55ff165d176034a864786aab8602dab035c229e5
Владислав Филатов <filatov@reg.ru>
2022-03-17 00:21:02 +0400

    Поправил нейминг в компоненте Tickets, разбил функцию в created на атомарные части

--- src/components/Tickets.vue
+++ src/components/Tickets.vue
@@ -1,63 +1,84 @@
 <template>
    <TicketsItem
-      v-for="ticket in tickets" 
-      v-bind:ticket="ticket"  
+      v-for="ticket in tickets"
+      :ticket="ticket"
       :key="ticket"
    />
 </template>
 
 <script>
-import TicketsItem from './TicketsItem.vue'
+import TicketsItem from './TicketsItem.vue';
 
-export default{
-   components:{
-      TicketsItem
+export default {
+   components: {
+      TicketsItem,
+   },
+
+   props: {
+      selectedTransfers: {
+         type: Array,
+         default: () => [],
+      }
    },
 
    data() {
       return {
          tickets: [],
+         filteredTickets: [],
          searchId: null,
-    }
+      };
    },
 
-   methods:{
-      change(){
-         this.tickets = this.tickets.map( ( item ) =>  {
+   // watch: {
+   //    selectedTransfers: function(newValue, oldValue) {
+   //       this.filteredTickets = this.tickets.filter(ticket => ticket)
+   //    },
+   // },
 
-            // Конструируем дату билета в одну сторону
-            const dateTicketThere = new Date(item.segments[0].date);
+   methods: {
+      async getSearchId() {
+         const response = await fetch('https://front-test.beta.aviasales.ru/search');
+         const { searchId } = await response.json();
+         this.searchId = searchId;
+      },
+
+      async getTickets() {
+         const response = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${this.searchId}`);
+         const { tickets } = await response.json();
+         this.tickets = tickets;
+      },
+
+      normalizeDate() {
+         this.tickets = this.tickets.map( ( ticket ) =>  {
+
+            // Нормализуем дату билета в одну сторону
+            const dateTicketThere = new Date(ticket.segments[0].date);
             const fullDateThere = `${dateTicketThere.getDate()}-${dateTicketThere.getMonth() + 1}-${dateTicketThere.getFullYear()}`;
-            item.segments[0].date = {
+            ticket.segments[0].date = {
                minutes: dateTicketThere.getMinutes(),
                hour: dateTicketThere.getHours(),
                fullDate: fullDateThere,
             };
 
-            // Конструируем дату билета в другую сторону
-            const dateTicketBack = new Date(item.segments[1].date);
+            // Нормализуем дату билета в другую сторону
+            const dateTicketBack = new Date(ticket.segments[1].date);
             const fullDateBack = `${dateTicketBack.getDate()}-${dateTicketBack.getMonth() + 1}-${dateTicketBack.getFullYear()}`;
-            item.segments[1].date = {
+            ticket.segments[1].date = {
                minutes: dateTicketBack.getMinutes(),
                hour: dateTicketBack.getHours(),
                fullDate: fullDateBack,
             };
 
-            return item;
+            return ticket;
          } );
       },
    },
    
    async created() {
-      const responseSearchId = await fetch('https://front-test.beta.aviasales.ru/search');
-      const { searchId } = await responseSearchId.json();
-      this.searchId = searchId;
-      const responseTikets = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${this.searchId}`);
-      const { tickets } = await responseTikets.json();
-      this.tickets = tickets;
+      await this.getSearchId();
+      await this.getTickets();
 
-      await this.change();
-      console.log(this.tickets);
+      this.normalizeDate();
     }
 }
 
