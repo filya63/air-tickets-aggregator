29c3706d3c11dbb35c27d74d053ed0ef146c71c8
Владислав Филатов <filatov@reg.ru>
2022-03-17 02:09:21 +0400

    Кастомизировал чекбокс в компоненте FilterTransfer

--- src/components/FilterTransfer.vue
+++ src/components/FilterTransfer.vue
@@ -7,9 +7,13 @@
             :key="key"
         >
             <input
+                class="transfer__item_checkbox"
                 type="checkbox"
                 @click="selectCheckbox( transfer )"
-            > {{ transfer }}
+            >
+            <span class="transfer__item_text">
+                {{ transfer }}
+            </span>
         </label>
     </form>
 </template>
@@ -57,6 +61,40 @@ export default {
         &__item {
             cursor: pointer;
             padding: 20px 20px;
+            display: flex;
+            align-items: center;
+            &_checkbox {
+                position: absolute;
+                z-index: -1;
+                opacity: 0;
+                &:checked+span::before {
+                    border-color: #0b76ef;
+                    background-color: #0b76ef;
+                    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+                }
+                &+span {
+                    display: inline-flex;
+                    align-items: center;
+                    user-select: none;
+                    &::before {
+                        content: '';
+                        display: inline-block;
+                        width: 1em;
+                        height: 1em;
+                        flex-shrink: 0;
+                        flex-grow: 0;
+                        border: 1px solid #adb5bd;
+                        border-radius: 0.25em;
+                        margin-right: 0.5em;
+                        background-repeat: no-repeat;
+                        background-position: center center;
+                        background-size: 50% 50%;
+                    }
+                }
+            }
+            &_text {
+                margin-left: 10px;
+            }
             &>input {
                 cursor: pointer;
             }
