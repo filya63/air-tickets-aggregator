2fcc8de7ef42c5c9b2d7ba39da682b296f5dfb1a
Владислав Филатов <filatov@reg.ru>
2022-03-23 01:29:22 +0400

    Порефакторил слой api

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,21 @@
 export default {
     getSearchId: async () => {
-        let response;
-
         try {
-            response = await fetch('https://front-test.beta.aviasales.ru/search');
+            const response = await fetch('https://front-test.beta.aviasales.ru/search');
+            const { searchId } = await response.json();
+            return searchId;
         } catch( error ) {
             throw new Error( error.message );
         }
-
-        const { searchId } = await response.json();
-        return searchId;
     },
 
     getTickets: async ( searchId ) => {
-        let response;
-
         try {
-            response = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`);
+            const response = await fetch(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`);
+            const { tickets } = await response.json();
+            return tickets;
         } catch( error ) {
             throw new Error( error.message );
         }
-
-        const { tickets } = await response.json();
-        return tickets;
     },
 }
