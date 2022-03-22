599dee583fcf453506c559d3d47fc90c0440f12e
Владислав Филатов <filatov@reg.ru>
2022-03-23 02:01:58 +0400

    Вынес url адрес для запросов в константу

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 export const SELECTED_TRANSFER_EVENT = 'selected-transfers';
 export const CHANGE_ACTIVE_TAB_EVENT = 'change-active-tab';
+export const AVIASALES_URL = 'https://front-test.beta.aviasales.ru';
