ad207ef84a763750b3b0be828c7499bb6473f556
Владислав Филатов <filatov@reg.ru>
2022-03-24 02:20:47 +0400

    Поправил табуляцию

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ export default {
         try {
             const response = await fetch(`${AVIASALES_URL}/search`);
             const { searchId } = await response.json();
+
             return searchId;
         } catch( error ) {
             throw new Error( error.message );
@@ -15,6 +16,7 @@ export default {
         try {
             const response = await fetch(`${AVIASALES_URL}/tickets?searchId=${searchId}`);
             const { tickets } = await response.json();
+            
             return tickets;
         } catch( error ) {
             throw new Error( error.message );
